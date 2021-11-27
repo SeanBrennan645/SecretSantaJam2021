31870542af36e8843fe09a09eb4b91e0c55912a6
SeanBrennan645 <seanbrennan645@gmail.com>
2021-11-27 18:00:59 +0000

    Added an Undertale style dialogue system

---
+++ Santas sEGGway/Assets/Scripts/Dialogue/DialogueBaseClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueBaseClass : MonoBehaviour
+    {
+        public bool isFinished { get; private set; }
+        protected IEnumerator WriteText(string input, Text TextHolder, float delay)
+        {
+            for(int i =0; i<input.Length; i++)
+            {
+                TextHolder.text += input[i];
+                yield return new WaitForSeconds(delay);
+            }
+
+            yield return new WaitUntil(() => Input.GetButtonDown("Jump")); //using jump cause cba to mess with unity input. Its spacebar anyway lol
+            isFinished = true;
+        }
+    }
+}
---
+++ Santas sEGGway/Assets/Scripts/Dialogue/DialogueLine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        [Header ("Text Options")]
+        [SerializeField] private string input;
+        //[SerializeField] private Color textColour;
+        //[SerializeField] private Font textFont;
+
+        [Header ("Time Parameters")]
+        [SerializeField] private float delay;
+        //[SerializeField] private float delayBetweenLines;
+
+        private Text textHolder;
+
+        private void Awake()
+        {
+            textHolder = GetComponent<Text>();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(WriteText(input, textHolder, delay));
+        }
+    }
+
+}

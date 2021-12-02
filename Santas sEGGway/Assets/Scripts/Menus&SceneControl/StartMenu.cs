c6c90e73dd63164c6fd123d7c0052352658bfac3
SeanBrennan645 <seanbrennan645@gmail.com>
2021-12-02 20:02:00 +0000

    Started work on main menu
    Just need to write out how to play info

---
+++ Santas sEGGway/Assets/Scripts/Menus&SceneControl/StartMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] SceneChanger sceneChanger = null;
+    [SerializeField] GameObject howToPlayUI = null;
+    [SerializeField] GameObject mainMenuUI = null;
+
+    public void StartGame()
+    {
+        sceneChanger.LoadNextScene();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void HowToPlay()
+    {
+        mainMenuUI.SetActive(false);
+        howToPlayUI.SetActive(true);
+    }
+
+    public void Back()
+    {
+        mainMenuUI.SetActive(true);
+        howToPlayUI.SetActive(false);
+    }
+}

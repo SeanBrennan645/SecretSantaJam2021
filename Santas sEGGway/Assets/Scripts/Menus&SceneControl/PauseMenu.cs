087074d511cdf7101bfdb065432bbed84b1f3ff4
SeanBrennan645 <seanbrennan645@gmail.com>
2021-12-02 18:28:06 +0000

    Did some reorganising and added a pause menu prefab

---
+++ Santas sEGGway/Assets/Scripts/Menus&SceneControl/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    [SerializeField] GameObject PauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        //Debug.Log("Quitting game!");
+        Application.Quit();
+    }
+}
